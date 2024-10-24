db2aa991e59e1468096516fe8b06d77fd4b9c361
Soundarya <34751165+sound25@users.noreply.github.com>
2024-10-24 12:24:16 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
 }
 
+resource "aws_s3_bucket_versioning" "insecure-bucket-versioning" {
+  bucket = aws_s3_bucket.insecure-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
 # resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
 #   block_public_acls       = true
 #   block_public_policy     = true
